f64affb9614cb5db315677f66ac808cba4d48bf2
aneesha22 <42198319+aneesha22@users.noreply.github.com>
2018-08-09 22:35:05 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
